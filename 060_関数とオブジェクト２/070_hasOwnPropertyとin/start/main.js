fd62afde2c5f64e5a654a1dc5870cb89a12b1843
nishimurasyota <syota4723@gmail.com>
2022-04-22 19:44:15 +0900

    :sparkles: hasOwnPropertyとinの違い

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -1,10 +1,17 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-Object.prototype.hello = function() {
-    console.log('Object: hello ' + this.name);
-}
+Object.prototype.hello = function () {
+  console.log("Object: hello " + this.name);
+};
 
-const bob = new Person('Bob', 18);
+const bob = new Person("Bob", 18);
+
+const res = Object.prototype.hasOwnProperty("hello");
+console.log(res);
+// hasOwnPropertyはそのオブジェクトがメソッドを持っているかを判断する
+
+// inはプロトタイプチェーンを遡って判断する
+console.log("hello" in bob);
