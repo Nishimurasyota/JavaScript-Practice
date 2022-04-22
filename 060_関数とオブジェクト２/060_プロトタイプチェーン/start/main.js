7da6a132a74353b01db6b55948a0874fac288cef
nishimurasyota <syota4723@gmail.com>
2022-04-22 19:31:27 +0900

    :sparkles: プロトタイプチェーンについて

--- 060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
+++ 060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
@@ -1,11 +1,25 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  this.hello = function () {
+    console.log("Own: hello " + this.name);
+  };
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
-}
+Person.prototype.hello = function () {
+  console.log("P: hello " + this.name);
+};
 
-const bob = new Person('Bob', 18);
+Object.prototype.hello = function () {
+  console.log("O: hello " + this.name);
+};
+
+// 実行する際に優先順位があり、浅い順から遡って探すようになっている
+
+const bob = new Person("Bob", 18);
 bob.hello();
+
+// hasOwnPropertyはbobにもPersonにもなくてObjectにしかない
+// プロトタイプチェーンを遡って検索して使用している
+const result = bob.hasOwnProperty("name");
+console.log(result);
