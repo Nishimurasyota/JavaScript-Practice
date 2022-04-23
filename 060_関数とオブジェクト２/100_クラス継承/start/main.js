ee3e559a2831db8d4ab883f48feb52c36940edc0
nishimurasyota <syota4723@gmail.com>
2022-04-23 21:20:08 +0900

    :sparkles: クラスの継承について

--- 060_関数とオブジェクト２/100_クラス継承/start/main.js
+++ 060_関数とオブジェクト２/100_クラス継承/start/main.js
@@ -1,25 +1,34 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log("hello " + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+class Japanese extends Person {
+  constructor(name, age, gender) {
+    super(name, age);
+    // superで親クラスのコンストラクターを参照している
+    this.gender = gender;
+  }
+  // helloはPersonでもあるのでextendsで継承されているため使用可能
+  // 上書きしているのでJapanese側で定義したメソッドが実行される
+  hello() {
+    console.log("Konnichiwa " + this.name);
+  }
+
+  bye() {
+    console.log("Sayonara " + this.name);
+  }
 }
 
-function Japanese(name, age, gender) {
-  Person.call(this, name, age);
-  this.gender = gender;
-}
+const bob = new Person("Bob", 24);
+console.log(bob);
 
-Japanese.prototype = Object.create(Person.prototype);
-
-Japanese.prototype.hello = function() {
-  console.log('Konnichiwa ' + this.name);
-}
-
-Japanese.prototype.bye = function() {
-  console.log('Sayonara ' + this.name);
-}
-
-const taro = new Japanese('Taro', 23, 'Male');
+const taro = new Japanese("taro", 21, "Male");
+console.log(taro);
+taro.hello();
