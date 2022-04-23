ecf1871a226e0f7d35c038837f8b1294ff8b16c0
nishimurasyota <syota4723@gmail.com>
2022-04-23 20:22:12 +0900

    :sparkles: プロトタイプ継承について

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -3,6 +3,19 @@ function Person(name, age) {
   this.age = age;
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+Person.prototype.hello = function () {
+  console.log("hello " + this.name);
+};
+
+const taro = new Person("taro", 20);
+console.log(taro.name);
+
+function Japanese(name, age) {
+  Person.call(this, name, age);
 }
+
+Japanese.prototype = Object.create(Person.prototype);
+
+const jiro = new Japanese("jiro", 12);
+console.log(jiro);
+jiro.hello();
