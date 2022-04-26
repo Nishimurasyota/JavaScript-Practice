9825831f44d1dd3ab1c54e2b5bfb487cdbd0819d
nishimurasyota <syota4723@gmail.com>
2022-04-26 15:23:05 +0900

    :sparkles: getter/setter/staticについて

--- 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -1,4 +1,41 @@
 function Person1(name, age) {
-	this.name = name;
-	this.age = age;
+  this._name = name;
+  this.age = age;
 }
+
+Object.defineProperty(Person1.prototype, "name", {
+  get: function () {
+    return this._name;
+  },
+  set: function (val) {
+    this._name = val;
+  },
+});
+
+const p1 = new Person1("tom", 14);
+
+p1.name = "Tom";
+
+console.log(p1.name);
+
+class Person2 {
+  // classでの書き方
+  constructor(name, age) {
+    this._name = name;
+    this.age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  static hello() {
+    console.log("Hello");
+  }
+}
+
+Person2.hello();
