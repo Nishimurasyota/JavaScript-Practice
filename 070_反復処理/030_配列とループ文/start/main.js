cede18be6a982bcf2e8a9d03d2b27f176d53b7c0
nishimurasyota <syota4723@gmail.com>
2022-04-28 14:34:49 +0900

    :sparkles: 配列とループ文

--- 070_反復処理/030_配列とループ文/start/main.js
+++ 070_反復処理/030_配列とループ文/start/main.js
@@ -1,1 +1,17 @@
-const arry = [1, 2, 3, 4, 5];
+const array = [1, 2, 3, 4, 5];
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// 配列にfalsyな値がある場合は使える（表示される）
+// let i = 0;
+// while (i < array.length) {
+//   console.log(array[i]);
+//   i++;
+// }
+
+// 配列にfalsyな値がある場合はfalsyな値の前までが表示される
+let v,i = 0;
+while ((v = array[i++])) {
+  console.log(v);
+}
