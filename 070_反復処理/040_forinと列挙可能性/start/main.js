200a64798aab79b7f34e5eee8ff444d221659e4f
nishimurasyota <syota4723@gmail.com>
2022-04-28 14:50:06 +0900

    :sparkles: for...inと列挙可能性

--- 070_反復処理/040_forinと列挙可能性/start/main.js
+++ 070_反復処理/040_forinと列挙可能性/start/main.js
@@ -1,5 +1,22 @@
+const s = Symbol();
+
 const obj = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3'
+  prop1: "value1",
+  prop2: "value2",
+  prop3: "value3",
+  [s]: "value4",
+};
+// Symbolは列挙対象にならない
+
+// Object.prototype.method = function(){}
+// Object.defineProperty(Object.prototype,"method",{
+// 	enumerable: false
+// })
+// definePropertyでenumerableをfalseにすると列挙対象にならない
+
+for (let key in obj) {
+  // hasOwnPropertyで条件をつけた場合、propは定義されているがprototypeは継承したものなので該当しない
+  if (obj.hasOwnProperty(key)) {
+    console.log(key, obj[key]);
+  }
 }
